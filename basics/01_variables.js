a030bc5dec877e2ffd3dd963e3a64962a925e843
Junaid Kambho <m.junaidarshad95@gmail.com>
2025-06-20 03:19:49 +0000

    config done

---
+++ basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID = 1234
+let   accountEmail = "junaid@google.com"
+var   accountPassword ="12345"
+accountCity ="pattoki"
+
+
+
+accountEmail = "awais@google.com"
+accountPassword ="121212"
+accountCity="lahore"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountID,accountEmail,accountPassword,accountCity])
